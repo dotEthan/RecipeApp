3ee419022f15a24df1adfedb101b4cef642764d4
Ethan Strauss <estrauss@gmail.com>
2021-01-03 19:55:02 -0500

    fix: shopping list editing works again

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 
-// import { NamedItem } from '../shared/namedItem.model';
 import * as fromShoppingList from './store/shopping-list.reducers';
 import * as ShoppingListActions from './store/shopping-list.actions';
-// import { ShoppingListService } from './shopping-list.service';
-// import * as fromApp from '../../app/store/app-reducer';
 import { WindowResizeService } from '../shared/window-resize.service';
 
 @Component({
@@ -32,14 +29,12 @@ export class ShoppingListComponent implements OnInit {
       this.defaultListIndex = state.defaultListIndex;
       this.shoppingListLength = state.shoppingLists.length;
       this.viewableListsIndexArray = state.viewableListIndexes;
-      console.log(state.shoppingLists);
     });
 
     this.windowResizeService.windowWidth.subscribe((width) => {
       this.windowWidth = width;
     });
 
-    console.log('widnow width: ', this.viewableListsIndexArray);
   }
   onSlButtonClick(listIndex) {
     this.store.dispatch(new ShoppingListActions.MaximizeViewableList(listIndex));
